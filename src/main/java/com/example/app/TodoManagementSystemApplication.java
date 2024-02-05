213e67a2b1283a2219e309e9725993b694e3ffad
Emiko ooya <evipunpunpunpun@gmail.com>
2024-02-05 11:25:42 +0900

    最初の状態

---
+++ src/main/java/com/example/app/TodoManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementSystemApplication.class, args);
+	}
+
+}
